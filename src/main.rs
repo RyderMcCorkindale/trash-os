c29ad9c663167881ce7d0b98d4b9e280becfb5a8
RyderMcCorkindale <rydermccorkindale@gmail.com>
2025-02-02 16:29:28 -0800

    Initial commit - added io function2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io;
+// use std::{thread, time};
+// use chrono::prelude::*;
+
+fn intake(x: &mut String) -> &String {
+    io::stdin()
+        .read_line(x)
+        .expect("couldn't read line");
+    x
 }
+
+#[allow(unused_mut)]
+fn main() {
+    // add an option for choosing whether to have 12 hour or military time, also to set timezone
+    // and date format
+    //Terminal Rust Ascii SHell Operating System
+    //dump command that dumps input
+
+
+    println!("[🗑] Welcome to TRASH OS! [🗑]");
+
+    let mut input = String::new();
+    loop {
+        intake(&mut input);
+        let mut _cmd = input.split_whitespace().next().unwrap();
+
+
+    }
+    //add a light that shows when a program is running
+}
+
+
