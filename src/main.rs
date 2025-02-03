f224319fd81d30657a3377680d47544c1ab4d6b5
RyderMcCorkindale <rydermccorkindale@gmail.com>
2025-02-03 03:40:24 -0800

    new aestetic changes and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 
 use std::io;
+use std::io::Write;
 // use std::{thread, time};
 // use chrono::prelude::*;
 
-fn intake(x: &mut String) -> &String {
+fn intake(x: &mut String) -> String {
+    //standard rust input 
     io::stdin()
         .read_line(x)
         .expect("couldn't read line");
-    x
+    
+    //trims the excess newline when you press enter
+    if x.ends_with('\n') {
+        x.pop();
+        if x.ends_with('\r') {
+            x.pop();
+        }
+    }
+    x.to_string()
 }
 
 #[allow(unused_mut)]
@@ -15,15 +25,24 @@ fn main() {
     // add an option for choosing whether to have 12 hour or military time, also to set timezone
     // and date format
     //Terminal Rust Ascii SHell Operating System
-    //dump command that dumps input
-
-
-    println!("[🗑] Welcome to TRASH OS! [🗑]");
+    //commands should be formated >cmd type
+    println!("=============================|");
+    println!("[🗑] welcome to TRASH OS!");
+    println!("[🗑] for viewing a list of commands, type >ls cmd");
+    println!("[🗑] you can also view a guide of the OS with >guide");
+    println!("=============================|");
 
     let mut input = String::new();
     loop {
+        print!("landfill:~$ ");
+        io::stdout().flush().unwrap();
         intake(&mut input);
-        let mut _cmd = input.split_whitespace().next().unwrap();
+        input.clear();
+
+
+        // let mut _cmd = input.split_whitespace().next().unwrap();
+
+
 
 
     }
